e73f2db3fe4ffde5285a7a690fe33b7176b460ae
Maggie Bupp <mvb6309@psu.edu>
2024-07-12 11:16:19 -0400

    trying UI out

--- src/component.py
+++ src/component.py
@@ -4,10 +4,13 @@ Template Component main class.
 """
 import csv
 import logging
+import pandas as pd
 from datetime import datetime
 
 from keboola.component.base import ComponentBase
 from keboola.component.exceptions import UserException
+from gretel_client import Gretel
+
 
 # configuration variables
 KEY_API_TOKEN = '#api_token'
@@ -37,59 +40,85 @@ class Component(ComponentBase):
         """
         Main execution code
         """
-
-        # ####### EXAMPLE TO REMOVE
-        # check for missing configuration parameters
-        self.validate_configuration_parameters(REQUIRED_PARAMETERS)
-        self.validate_image_parameters(REQUIRED_IMAGE_PARS)
         params = self.configuration.parameters
-        # Access parameters in data/config.json
-        if params.get(KEY_PRINT_HELLO):
-            logging.info("Hello World")
+        API_KEY=params['API_KEY']
+        gretel = Gretel(api_key=API_KEY, validate=True)
+        dataset_path_dict = {
+            "adult income in the USA (14000 records, 15 fields)": "https://raw.githubusercontent.com/gretelai/gretel-blueprints/main/sample_data/us-adult-income.csv",
+            "hospital length of stay (9999 records, 18 fields)": "https://raw.githubusercontent.com/gretelai/gretel-blueprints/main/sample_data/sample-synthetic-healthcare.csv",
+            "customer churn (7032 records, 21 fields)": "https://raw.githubusercontent.com/gretelai/gretel-blueprints/main/sample_data/monthly-customer-payments.csv"
+        }
 
-        # get input table definitions
-        input_tables = self.get_input_tables_definitions()
-        for table in input_tables:
-            logging.info(f'Received input table: {table.name} with path: {table.full_path}')
+        # dataset = "adult income in the USA (14000 records, 15 fields)" # @param ["adult income in the USA (14000 records, 15 fields)", "hospital length of stay (9999 records, 18 fields)", "customer churn (7032 records, 21 fields)"]
+        # dataset = dataset_path_dict[dataset]
+        dataset=params[dataset]
+        df = pd.read_csv(dataset)
+        df.head()
+        trained = gretel.submit_train("tabular-actgan", data_source=dataset)
 
-        if len(input_tables) == 0:
-            raise UserException("No input tables found")
+        # view the quality scores
+        print(trained.report)   
+        # display the full report within this notebook
+        trained.report.display_in_notebook()
+        # inspect the synthetic data used to create the report
+        df_synth_report = trained.fetch_report_synthetic_data()
+        df_synth_report.head()
+        generated = gretel.submit_generate(trained.model_id, num_records=1000)
+        # inspect the generated synthetic data
+        generated.synthetic_data.head()
+        
+        # # ####### EXAMPLE TO REMOVE
+        # # check for missing configuration parameters
+        # self.validate_configuration_parameters(REQUIRED_PARAMETERS)
+        # self.validate_image_parameters(REQUIRED_IMAGE_PARS)
+        # params = self.configuration.parameters
+        # # Access parameters in data/config.json
+        # if params.get(KEY_PRINT_HELLO):
+        #     logging.info("Hello World")
 
-        # get last state data/in/state.json from previous run
-        previous_state = self.get_state_file()
-        logging.info(previous_state.get('some_state_parameter'))
+        # # get input table definitions
+        # input_tables = self.get_input_tables_definitions()
+        # for table in input_tables:
+        #     logging.info(f'Received input table: {table.name} with path: {table.full_path}')
 
-        # Create output table (Tabledefinition - just metadata)
-        table = self.create_out_table_definition('output.csv', incremental=True, primary_key=['timestamp'])
+        # if len(input_tables) == 0:
+        #     raise UserException("No input tables found")
 
-        # get file path of the table (data/out/tables/Features.csv)
-        out_table_path = table.full_path
-        logging.info(out_table_path)
+        # # get last state data/in/state.json from previous run
+        # previous_state = self.get_state_file()
+        # logging.info(previous_state.get('some_state_parameter'))
 
-        # Add timestamp column and save into out_table_path
-        input_table = input_tables[0]
-        with (open(input_table.full_path, 'r') as inp_file,
-              open(table.full_path, mode='wt', encoding='utf-8', newline='') as out_file):
-            reader = csv.DictReader(inp_file)
+        # # Create output table (Tabledefinition - just metadata)
+        # table = self.create_out_table_definition('output.csv', incremental=True, primary_key=['timestamp'])
 
-            columns = list(reader.fieldnames)
-            # append timestamp
-            columns.append('timestamp')
+        # # get file path of the table (data/out/tables/Features.csv)
+        # out_table_path = table.full_path
+        # logging.info(out_table_path)
 
-            # write result with column added
-            writer = csv.DictWriter(out_file, fieldnames=columns)
-            writer.writeheader()
-            for in_row in reader:
-                in_row['timestamp'] = datetime.now().isoformat()
-                writer.writerow(in_row)
+        # # Add timestamp column and save into out_table_path
+        # input_table = input_tables[0]
+        # with (open(input_table.full_path, 'r') as inp_file,
+        #       open(table.full_path, mode='wt', encoding='utf-8', newline='') as out_file):
+        #     reader = csv.DictReader(inp_file)
 
-        # Save table manifest (output.csv.manifest) from the tabledefinition
-        self.write_manifest(table)
+        #     columns = list(reader.fieldnames)
+        #     # append timestamp
+        #     columns.append('timestamp')
 
-        # Write new state - will be available next run
-        self.write_state_file({"some_state_parameter": "value"})
+        #     # write result with column added
+        #     writer = csv.DictWriter(out_file, fieldnames=columns)
+        #     writer.writeheader()
+        #     for in_row in reader:
+        #         in_row['timestamp'] = datetime.now().isoformat()
+        #         writer.writerow(in_row)
 
-        # ####### EXAMPLE TO REMOVE END
+        # # Save table manifest (output.csv.manifest) from the tabledefinition
+        # self.write_manifest(table)
+
+        # # Write new state - will be available next run
+        # self.write_state_file({"some_state_parameter": "value"})
+
+        # # ####### EXAMPLE TO REMOVE END
 
 
 """

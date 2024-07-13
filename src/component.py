a55d9fdc7d1f9693d6d31cba812f0f73c78e40be
Maggie Bupp <mvb6309@psu.edu>
2024-07-12 21:33:37 -0400

    logged in w github

--- src/component.py
+++ src/component.py
@@ -52,6 +52,7 @@ class Component(ComponentBase):
         dataset = "adult income in the USA (14000 records, 15 fields)" # @param ["adult income in the USA (14000 records, 15 fields)", "hospital length of stay (9999 records, 18 fields)", "customer churn (7032 records, 21 fields)"]
         dataset = dataset_path_dict[dataset]
         #dataset=params['dataset']
+        #
         df = pd.read_csv(dataset)
         df.head()
         trained = gretel.submit_train("tabular-actgan", data_source=dataset)
